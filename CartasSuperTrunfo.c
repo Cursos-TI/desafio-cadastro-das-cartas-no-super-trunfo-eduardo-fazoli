c30483180c20026236bb766cd60a5e163fb2f457
eduardo <eduardofazolis@gmail.com>
2025-02-06 16:49:15 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+// eu nao tinha ctz se era pra usar somente printf e scanf, 
+// mas somente com scanf eu nao conseguir fazer com que ele lesse uma string com espacos
+// apos uma pesquisa de como usar o gets e como limpar o buffer para que ele rodasse corretamente
+// cheguei a esse resultado:
+
+
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char nomecidade[50];
+    int habitantes;
+    float area;
+    float pib;
+    int pontos;
+
+    printf("Digite o nome da cidade: ");
+    fgets(nomecidade, sizeof(nomecidade), stdin);
+
+
+    if (nomecidade[strlen(nomecidade) - 1] == '\n') {
+        nomecidade[strlen(nomecidade) - 1] = '\0';
+    }
+
+  
+    printf("Digite o número de habitantes: ");
+    scanf("%d", &habitantes);
+    limparBuffer();
+
+    printf("Digite a área da cidade (em km²): ");
+    scanf("%f", &area);
+    limparBuffer(); 
+
+    printf("Digite o PIB da cidade (em R$): ");
+    scanf("%f", &pib);
+    limparBuffer();
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontos);
+    limparBuffer(); 
+
+    printf("\nInformações da cidade:\n");
+    printf("Nome da cidade: %s\n", nomecidade);
+    printf("Número de habitantes: %d\n", habitantes);
+    printf("Área da cidade: %.2f km²\n", area);
+    printf("PIB da cidade: R$ %.2f\n", pib);
+    printf("Número de pontos turísticos: %d\n", pontos);
 
     return 0;
 }
